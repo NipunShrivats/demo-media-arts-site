a45c353926f0701957babffebd44e8cdbf185262
NipunShrivats <nipunrawat16@gmail.com>
2024-09-20 17:50:13 +0530

    changes on 20sep, commit no.3

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import "./NavbarStyle.css"
-import logow from "../../assets/imgAssets/logo-white.png"
-import logob from "../../assets/imgAssets/logo-black.png"
-import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll';
-import MenuIcon from "../../assets/imgAssets/menu-icon.png"
+import React, { useEffect, useState } from "react";
+import "./NavbarStyle.css";
+import logow from "../../assets/imgAssets/logo-white.png";
+import logob from "../../assets/imgAssets/logo-black.png";
+import {
+  Link,
+  Button,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+} from "react-scroll";
+import MenuIcon from "../../assets/imgAssets/menu-icon.png";
 
-import Logo from "../../assets/imgAssets/logo.png"
-
+import Logo from "../../assets/imgAssets/logo-main.png";
 
 export default function Navbar() {
+  const [sticky, setSticky] = useState(false);
 
-    const [sticky, setSticky] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 100 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            window.scrollY > 100 ? setSticky(true) : setSticky(false);
-        })
-    }, [])
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  };
 
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const toggleMenu = () => {
-        mobileMenu ? setMobileMenu(false) : setMobileMenu(true)
-    }
-
-    return (
-        <>
-            <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
-                {/* <div className='logo-cover1'>
+  return (
+    <>
+      <nav className={`container ${sticky ? "dark-nav" : ""}`}>
+        {/* <div className='logo-cover1'>
                     <div className='logo-inside1'>
                         <img src={logow} className='logo' alt="" />
                     </div>
@@ -36,35 +41,47 @@ export default function Navbar() {
                     </div>
                 </div> */}
 
-                <div className='logo-cover1'>
-                    <img src={Logo} alt="" className='logo-img' />
-                </div>
+        <div className="logo-cover1">
+          <img src={Logo} alt="" className="logo-img" />
+        </div>
 
-                {/* <span><RxHamburgerMenu className='menu-icon' onClick={toggleMenu} /></span> */}
-                <img src={MenuIcon} alt="" className='menu-icon' onClick={toggleMenu} />
-                <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
-                    <li><Link to='hero'
-                        smooth={true}
-                        offset={0}
-                        durastion={500}>Home</Link></li>
-                    <li><Link to='services'
-                        smooth={true}
-                        offset={-290}
-                        durastion={500}>Services</Link></li>
-                    <li> <Link to='about'
-                        smooth={true}
-                        offset={-240}
-                        durastion={500}>About Us</Link></li>
-                    <li><Link to='directors'
-                        smooth={true}
-                        offset={-210}
-                        durastion={500}>Directors</Link></li>
-                    <li className='btn-li'><Link to='contact'
-                        smooth={true}
-                        offset={-250}
-                        durastion={500} className='btn'>Contact Us</Link> </li>
-                </ul>
-            </nav >
-        </>
-    )
+        {/* <span><RxHamburgerMenu className='menu-icon' onClick={toggleMenu} /></span> */}
+        <img src={MenuIcon} alt="" className="menu-icon" onClick={toggleMenu} />
+        <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
+          <li>
+            <Link to="hero" smooth={true} offset={0} durastion={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="services" smooth={true} offset={-290} durastion={500}>
+              Services
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="about" smooth={true} offset={-240} durastion={500}>
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link to="directors" smooth={true} offset={-210} durastion={500}>
+              Directors
+            </Link>
+          </li>
+          <li className="btn-li">
+            <Link
+              to="contact"
+              smooth={true}
+              offset={-250}
+              durastion={500}
+              className="btn"
+            >
+              Contact Us
+            </Link>{" "}
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
