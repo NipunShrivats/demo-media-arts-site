951a5683384afa0a6d00757c736dfb4cd8f30b15
NipunShrivats <nipunrawat16@gmail.com>
2024-09-20 17:53:14 +0530

    changes on 20sep, commit no.4

--- src/Pages/LogoNav/LogoNav.jsx
+++ src/Pages/LogoNav/LogoNav.jsx
@@ -1,18 +1,18 @@
-import { React } from 'react'
-import "./LogoNavStyle.css"
-import { Link } from "react-router-dom"
-import Logo from "../../assets/imgAssets/logo.png"
+import { React } from "react";
+import "./LogoNavStyle.css";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/imgAssets/logo-main.png";
 
 export default function Nav() {
-
-
-    return (
-        <>
-            <div className={`nav container`}>
-                <div className='NavLogoCover'>
-                    <Link to="/"><img src={Logo} className="img" alt="" /></Link>
-                </div>
-            </div >
-        </>
-    )
+  return (
+    <>
+      <div className={`nav container`}>
+        <div className="NavLogoCover">
+          <Link to="/">
+            <img src={Logo} className="img" alt="" />
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
