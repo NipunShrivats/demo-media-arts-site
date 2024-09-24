c85f1938db5784825caa18934c8347c899ae031c
NipunShrivats <nipunrawat16@gmail.com>
2024-09-24 17:57:15 +0530

    changes on 24sep, commit no.1

--- src/Components/Directors/Directors.jsx
+++ src/Components/Directors/Directors.jsx
@@ -11,8 +11,8 @@ export default function Directors() {
         <Card
           img={Harsh}
           name={"Mr. Harsh Anand"}
-          about1={`A Film Director, started way back in 2003, worked with top production houses like balaji telefilms, venus records and tapes, Jai mehta productions.`}
-          about2={`Directed one of the top serial of indian television “kyunki saas bhi kabhi bahu thi”, directed film Starring Ravi kishan won best debut director award in dubai, even on OTT you can see his work on "MX Player", "Hungama" and many more. He is known for his creative genius. He also holds an important position as The Managing Editor of "The Bollywood Faces" Magazine,`}
+          about1={`A Film Director, started way back in 2003, worked with top production houses like Balaji Telefilms, Venus Records and Tapes, Jai Mehta Productions.`}
+          about2={`Directed one of the top serial of Indian television “Kyunki Saas Bhi Kabhi Bahu Thi”, directed film Starring Ravi kishan won best debut director award in Dubai, even on OTT you can see his work on "MX Player", "Hungama" and many more. He is known for his creative genius. He also holds an important position as The Managing Editor of "The Bollywood Faces" Magazine,`}
           about3={`Besides being the backbone of the company, he is also a successful film producer- producing feature film "Love aur Rajneeti" starring Ravi Kishan, short films starring Hiten Tejwani, Arun Bakshi, Manoj Bakshi and the list continues.`}
         />
         <Card
